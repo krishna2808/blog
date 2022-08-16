bb8375675de4b4e3e7b0fc9da864a66b846e0ebd
krishna2808 <kk8140631@gmail.com>
2022-08-16 23:08:32 +0530

    commited

--- post/views.py
+++ post/views.py
@@ -1,7 +1,7 @@
 #class base view 
 
 
-from turtle import title
+
 from django.shortcuts import render
 from django.http import HttpResponse , HttpResponseRedirect
 from .forms import AddPost, EditPost
